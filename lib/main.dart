456c97df2140833698b2b5cf8fb788f2d7047c09
Rezende-Fabio <rezendefabio337@gmail.com>
2023-03-19 11:26:13 -0300

    style modal transection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:despesas/models/transection.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(Main());
 
@@ -37,7 +38,6 @@ class MyHome extends StatelessWidget {
         title: Text("Despesas Pessoais"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -51,49 +51,36 @@ class MyHome extends StatelessWidget {
           Column(
             children: _transaction.map((tr) {
               return Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10
-                ),
+                margin: EdgeInsets.symmetric(vertical: 10),
                 child: Row(
                   children: [
                     Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2
-                        ),
-                        borderRadius: BorderRadius.circular(50)
-                      ),
+                          border: Border.all(color: Colors.purple, width: 2),
+                          borderRadius: BorderRadius.circular(50)),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        "R\$ ${tr.value.toStringAsFixed(2)}",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                           color: Colors.purple,
                         ),
-                        ),
+                      ),
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Text(tr.title,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold)),
                         Text(
-                          tr.title,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold
-                            )
-                          ),
-                        Text(
-                          tr.date.toString(),
-                          style: TextStyle(
-                            color: Colors.grey
-                          ),
+                          DateFormat("d MMM y HH:MM").format(tr.date),
+                          style: TextStyle(color: Colors.grey),
                         ),
                       ],
                     )
@@ -101,6 +88,35 @@ class MyHome extends StatelessWidget {
                 ),
               );
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Titulo"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Valor (R\$)"),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        child: Text("Nova Transação"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.purple
+                        ),
+                        onPressed: () => {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
